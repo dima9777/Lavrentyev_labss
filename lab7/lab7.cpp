0c21f9ed61d5eacf80d3c8568b600c510b72cfc8
dima9777 <dinamo9777@gmail.com>
2016-06-04 00:18:02 +0300

    Create lab7.cpp

---
+++ lab7/lab7.cpp
@@ -0,0 +1,67 @@
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include "Windows.h"
+
+int main() 
+{
+	std::string c;
+	std::cout << "If you want to work with the queue, enter 'qlib',\nwith a priority queue, enter 'qwplib',\nelse any key: ";
+	std::cin >> c;
+	while (c == "qlib" || c == "qwplib") 
+	{
+		HINSTANCE h=nullptr;
+		if (c == "qlib") h = LoadLibrary(L"qlib");
+		if (c == "qwplib") h = LoadLibrary(L"qwplib");
+		if (h) 
+		{
+			int n;
+			std::cin >> n;
+			for (int i = 0; i < n; i++) 
+			{
+				std::string b;
+				int c;
+				std::cin >> b;
+
+				if (b == "add") 
+				{
+					std::cin >> c;
+					typedef void s(int b);
+					FARPROC add = (FARPROC)GetProcAddress((HMODULE)h, "add");
+					((s*)add)(c);
+				}
+
+				if (b == "del") 
+				{
+					FARPROC del = (FARPROC)GetProcAddress((HMODULE)h, "del");
+					try 
+					{
+						del();
+					}
+					catch (const char* a) 
+					{
+						std::cout << a;
+					}
+				}
+
+				if (b == "get") 
+				{
+					FARPROC get = (FARPROC)GetProcAddress((HMODULE)h, "get");
+					try {
+						std::cout << get() << std::endl;
+					}
+					catch (const char* a) 
+					{
+						std::cout << a;
+					}
+				}
+			}
+			FreeLibrary(h);
+		}
+		else std::cout << "Can't load library!\n";
+		std::cout << "If you want to continue the work, please re-select queue type,\nelse any key: ";
+		std::cin >> c;
+	}
+	system("pause");
+	return 0;
+}
